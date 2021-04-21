f0f2c99511ad57e5508d140086b50812af534928
kimbokChi <han15050424@gmail.com>
2021-04-21 09:07:10 +0900

    애플리케이션 창의 이름을 설정한다
    설정한 이름은 "GoogleSheet To Json"이다.

--- GoogleSheet2Json/Form1.Designer.cs
+++ GoogleSheet2Json/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace GoogleSheet2Json
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "GoogleSheet To Json";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
